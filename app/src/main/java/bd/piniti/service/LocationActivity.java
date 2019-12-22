2da5591b158e535c708692426541ae64a915b20a
animash-cse <animash.cse@gmail.com>
2019-12-23 03:30:00 +0600

    Create Map Activity

--- app/src/main/java/bd/piniti/service/LocationActivity.java
+++ app/src/main/java/bd/piniti/service/LocationActivity.java
@@ -5,22 +5,34 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
 import android.view.View;
+import android.widget.TextView;
+
+import bd.piniti.service.location.GetLocation;
 
 public class LocationActivity extends AppCompatActivity {
 
 
-    CardView cardview;
+    private CardView cardview;
+    private TextView location;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
 
+        location = findViewById(R.id.detect_my_location);
         cardview = findViewById(R.id.cardview);
         cardview.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LocationActivity.this,SearchingActivity.class);
+                Intent intent = new Intent(LocationActivity.this, SearchingActivity.class);
+                startActivity(intent);
+            }
+        });
+        location.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LocationActivity.this, GetLocation.class);
                 startActivity(intent);
             }
         });
---
+++ app/src/main/java/bd/piniti/service/location/GetLocation.java
@@ -0,0 +1,96 @@
+package bd.piniti.service.location;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import bd.piniti.service.R;
+
+public class GetLocation extends FragmentActivity implements OnMapReadyCallback {
+
+    private static final int PERMISSIONS_REQUEST = 100;
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.get_location);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+
+        //Check whether GPS tracking is enabled//
+        LocationManager lm = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if (!lm.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            finish();
+        }
+
+        //Check whether this app has access to the location permission//
+        int permission = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION);
+
+
+        //If the location permission has been granted, then start the TrackerService//
+        if (permission == PackageManager.PERMISSION_GRANTED) {
+           // startTrackerService();
+        } else {
+
+        //If the app doesn’t currently have access to the user’s location, then request access//
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+
+        //If the permission has been granted...//
+        if (requestCode == PERMISSIONS_REQUEST && grantResults.length == 1
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+            //...then start the GPS tracking service//
+           // startTrackerService();
+        } else {
+
+            //If the user denies the permission request, then display a toast with some more information//
+            Toast.makeText(this, "Please enable location services to allow GPS tracking", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    //Start the TrackerService//
+    private void startTrackerService() {
+        startService(new Intent(this, TrackingService.class));
+
+        //Notify the user that tracking has been enabled//
+        Toast.makeText(this, "GPS tracking enabled", Toast.LENGTH_SHORT).show();
+        //Close Activity//
+        finish();
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+}
